1dafb42869142819d144e4e1a01677cb62e016fa
tvanantwerp <tom@tomvanantwerp.com>
2019-03-08 14:45:17 -0500

    Initial commit

---
+++ gatsby-config.js
@@ -0,0 +1,36 @@
+module.exports = {
+  siteMetadata: {
+    title: `Gatsby Default Starter`,
+    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
+    author: `@gatsbyjs`,
+  },
+  plugins: [
+    `gatsby-plugin-styled-components`,
+    `gatsby-plugin-react-helmet`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images`,
+      },
+    },
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `Tax Prom 2019`,
+        short_name: `Tax Prom`,
+        start_url: `/`,
+        background_color: `#0094ff`,
+        theme_color: `#0094ff`,
+        display: `minimal-ui`,
+        icon: `src/images/tf-logo.png`,
+        include_favicon: true,
+      },
+    },
+    // this (optional) plugin enables Progressive Web App + Offline functionality
+    // To learn more, visit: https://gatsby.dev/offline
+    // 'gatsby-plugin-offline',
+  ],
+}
