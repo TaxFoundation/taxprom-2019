e6608780e02eef9d15b5c7954e755be26ca8a525
Tom VanAntwerp <tom@tomvanantwerp.com>
2019-03-13 13:19:56 -0400

    Organize previous sponsors by level

--- src/sections/PreviousSponsors.js
+++ src/sections/PreviousSponsors.js
@@ -9,8 +9,11 @@ const PreviousSponsors = () => {
       allPreviousSponsorsYaml {
         edges {
           node {
-            name
-            link
+            level
+            sponsors {
+              name
+              link
+            }
           }
         }
       }
@@ -18,27 +21,32 @@ const PreviousSponsors = () => {
   `);
 
   return (
-    <ul>
-      {data.allPreviousSponsorsYaml.edges
-        .sort((a, b) => {
-          // Chrome only accepts -1, 0, and 1 as sort comparison return values
-          if (a.node.name > b.node.name) return 1;
-          return -1;
-        })
-        .map(({ node: sponsor }) =>
-          sponsor.link ? (
-            <li key={`previous-sponsor-${slugify(sponsor.name)}`}>
-              <a href={sponsor.link} rel="noopener noreferrer" target="_blank">
-                {sponsor.name}
-              </a>
-            </li>
-          ) : (
-            <li key={`previous-sponsor-${slugify(sponsor.name)}`}>
-              {sponsor.name}
-            </li>
-          )
-        )}
-    </ul>
+    <>
+      {data.allPreviousSponsorsYaml.edges.map(({ node }) => (
+        <>
+          <h4>{node.level}</h4>
+          <ul>
+            {node.sponsors.map(sponsor =>
+              sponsor.link ? (
+                <li key={`previous-sponsor-${slugify(sponsor.name)}`}>
+                  <a
+                    href={sponsor.link}
+                    rel="noopener noreferrer"
+                    target="_blank"
+                  >
+                    {sponsor.name}
+                  </a>
+                </li>
+              ) : (
+                <li key={`previous-sponsor-${slugify(sponsor.name)}`}>
+                  {sponsor.name}
+                </li>
+              )
+            )}
+          </ul>
+        </>
+      ))}
+    </>
   );
 };
 
