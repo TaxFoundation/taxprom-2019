cdf4c4f2f8b7c548aba6583643181d5727d14af2
Tom VanAntwerp <tom@tomvanantwerp.com>
2019-03-14 15:51:05 -0400

    Improve ticket listing styles

--- src/components/Ticket.js
+++ src/components/Ticket.js
@@ -2,6 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
 import { Link } from 'gatsby';
+
 import SponsorLevel from './SponsorLevel';
 import { dollars, slugify } from '../utilities/formatters';
 
@@ -9,13 +10,39 @@ const TicketBox = styled(SponsorLevel)`
   background-color: ${props => props.theme[props.bg]};
   color: ${props => props.theme[props.color]};
 
+  .sponsorship__title {
+    color: ${props => props.theme[props.color]};
+    font-family: ${props => props.theme.fontFamilies.lato};
+    font-size: calc(2rem + 1.5vw);
+  }
+
   .sponsorship__price {
-    border-bottom: 1px solid ${props => props.theme[props.color]};
+    border: 0;
+    font-family: ${props => props.theme.fontFamilies.baskerville};
+    font-size: 1.6rem;
+    font-style: italic;
+    padding: 0;
+  }
+
+  .sponsorship__benefits {
+    border-top: 1px solid ${props => props.theme[props.color]};
+    padding-top: 1rem;
+  }
+
+  .sponsorship__pledge {
+    background-color: ${props => props.theme[props.color]};
+    color: ${props => props.theme[props.bg]};
+
+    &:active,
+    &:focus,
+    &:visited {
+      color: ${props => props.theme[props.bg]};
+    }
   }
 `;
 
 const Ticket = ({ ticket, id }) => (
-  <TicketBox>
+  <TicketBox bg={ticket.bg} color={ticket.color}>
     <h4 className="sponsorship__title">{ticket.name}</h4>
     <p className="sponsorship__price">{dollars(ticket.price)}</p>
     <ul className="sponsorship__benefits">
@@ -35,7 +62,6 @@ const Ticket = ({ ticket, id }) => (
 Ticket.propTypes = {
   ticket: PropTypes.object,
   id: PropTypes.string,
-  price: PropTypes.number,
 };
 
 export default Ticket;
--- src/sections/Tickets.js
+++ src/sections/Tickets.js
@@ -46,6 +46,8 @@ const Tickets = () => {
           node {
             name
             price
+            bg
+            color
             benefits
           }
         }
