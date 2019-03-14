efc8a05fa377d6d93c08526fbc2764df46412ffd
Tom VanAntwerp <tom@tomvanantwerp.com>
2019-03-14 14:52:53 -0400

    Use gatsby image for photo grids

--- src/sections/Details.js
+++ src/sections/Details.js
@@ -1,6 +1,8 @@
 import React, { Fragment } from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
+import { graphql, useStaticQuery } from 'gatsby';
+import Img from 'gatsby-image';
 
 import BackgroundColorContainer from '../components/BackgroundContainer';
 import SectionContainer from '../components/SectionContainer';
@@ -69,68 +71,107 @@ const PhotoGrid = styled.div`
   }
 `;
 
-const Photo = styled.div`
-  background-image: url(${props => props.src});
-  background-size: cover;
-  background-position: 50% 50%;
+const Photo = styled(Img)`
+  background-color: ${props => props.theme.black};
+
+  & > img {
+    object-fit: cover !important;
+    object-position: 50% 50% !important;
+    font-family: 'object-fit: cover !important; object-position: 50% 50% !important;';
+  }
 `;
 
-const Details = ({ details, id }) => (
-  <Fragment>
-    <BackgroundColorContainer bg="black" color="white">
-      <TaglineContainer>
-        <h2>Celebrating 82 Years of Promoting Better Tax Policy</h2>
-        <p>
-          Since our founding in 1937, the Tax Foundation has been the nation’s
-          leading independent tax policy nonprofits. Our principled research,
-          insightful analysis, and engaged experts have informed smarter tax
-          policy at the federal, state, and global levels.
-        </p>
-        <p>
-          Our aim in our interactions with policymakers at every level is to
-          guide them towards tax policies that are pro-growth, pro-investment,
-          pro-jobs, and hospitable to the private sector.
-        </p>
-        <p>
-          For 82 years, the Tax Foundation’s annual dinner has been an
-          institution for the tax community’s best, brightest, and most
-          influential. Join us Thursday, November 21st for our 82nd annual Tax
-          Prom, and find out why hundreds of people say this is the tax world’s
-          most celebrated event of the year.
-        </p>
-      </TaglineContainer>
-    </BackgroundColorContainer>
-    <BackgroundColorContainer bg="black" color="white">
-      <PhotoGrid>
-        <Photo src={PhotoA} />
-        <Photo src={PhotoB} />
-        <Photo src={PhotoC} />
-        <Photo src={PhotoD} />
-      </PhotoGrid>
-    </BackgroundColorContainer>
-    <BackgroundColorContainer bg="black" color="white">
-      <div id={id}>
-        <SectionContainer>
-          <h3>Why You Can't Miss Tax Prom</h3>
-          <DetailsList>
-            {details.detailsBullets.map((d, i) => (
-              <li key={`detail-${i}`}>{d}</li>
-            ))}
-          </DetailsList>
-          <h4>In 2018 Tax Prom was Joined By</h4>
-          <StatisticsList statsNumber={details.detailsStatistics.length}>
-            {details.detailsStatistics.map((d, i) => (
-              <StatisticItem key={`stat-${i}`} color={d.color}>
-                <StatisticsNumber>{d.number}</StatisticsNumber>
-                <StatisticsText>{d.text}</StatisticsText>
-              </StatisticItem>
-            ))}
-          </StatisticsList>
-        </SectionContainer>
-      </div>
-    </BackgroundColorContainer>
-  </Fragment>
-);
+const Details = ({ details, id }) => {
+  const images = useStaticQuery(graphql`
+    query DetailsImages {
+      hodge: file(relativePath: { eq: "hodge-speaks.jpg" }) {
+        childImageSharp {
+          fluid(maxWidth: 800, quality: 80) {
+            ...GatsbyImageSharpFluid_withWebp
+          }
+        }
+      }
+      angus: file(relativePath: { eq: "angus-speaks.jpg" }) {
+        childImageSharp {
+          fluid(maxWidth: 800, quality: 80) {
+            ...GatsbyImageSharpFluid_withWebp
+          }
+        }
+      }
+      table: file(relativePath: { eq: "table.jpg" }) {
+        childImageSharp {
+          fluid(maxWidth: 800, quality: 80) {
+            ...GatsbyImageSharpFluid_withWebp
+          }
+        }
+      }
+      threeMen: file(relativePath: { eq: "three-men.jpg" }) {
+        childImageSharp {
+          fluid(maxWidth: 800, quality: 80) {
+            ...GatsbyImageSharpFluid_withWebp
+          }
+        }
+      }
+    }
+  `);
+
+  return (
+    <>
+      <BackgroundColorContainer bg="black" color="white">
+        <TaglineContainer>
+          <h2>Celebrating 82 Years of Promoting Better Tax Policy</h2>
+          <p>
+            Since our founding in 1937, the Tax Foundation has been the nation’s
+            leading independent tax policy nonprofits. Our principled research,
+            insightful analysis, and engaged experts have informed smarter tax
+            policy at the federal, state, and global levels.
+          </p>
+          <p>
+            Our aim in our interactions with policymakers at every level is to
+            guide them towards tax policies that are pro-growth, pro-investment,
+            pro-jobs, and hospitable to the private sector.
+          </p>
+          <p>
+            For 82 years, the Tax Foundation’s annual dinner has been an
+            institution for the tax community’s best, brightest, and most
+            influential. Join us Thursday, November 21st for our 82nd annual Tax
+            Prom, and find out why hundreds of people say this is the tax
+            world’s most celebrated event of the year.
+          </p>
+        </TaglineContainer>
+      </BackgroundColorContainer>
+      <BackgroundColorContainer bg="black" color="white">
+        <PhotoGrid>
+          <Photo fluid={images.hodge.childImageSharp.fluid} />
+          <Photo fluid={images.angus.childImageSharp.fluid} />
+          <Photo fluid={images.table.childImageSharp.fluid} />
+          <Photo fluid={images.threeMen.childImageSharp.fluid} />
+        </PhotoGrid>
+      </BackgroundColorContainer>
+      <BackgroundColorContainer bg="black" color="white">
+        <div id={id}>
+          <SectionContainer>
+            <h3>Why You Can't Miss Tax Prom</h3>
+            <DetailsList>
+              {details.detailsBullets.map((d, i) => (
+                <li key={`detail-${i}`}>{d}</li>
+              ))}
+            </DetailsList>
+            <h4>In 2018 Tax Prom was Joined By</h4>
+            <StatisticsList statsNumber={details.detailsStatistics.length}>
+              {details.detailsStatistics.map((d, i) => (
+                <StatisticItem key={`stat-${i}`} color={d.color}>
+                  <StatisticsNumber>{d.number}</StatisticsNumber>
+                  <StatisticsText>{d.text}</StatisticsText>
+                </StatisticItem>
+              ))}
+            </StatisticsList>
+          </SectionContainer>
+        </div>
+      </BackgroundColorContainer>
+    </>
+  );
+};
 
 Details.propTypes = {
   details: PropTypes.object,
