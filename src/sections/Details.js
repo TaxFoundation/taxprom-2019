de0752e2c227cc34d163c970b0ce583ba47b9950
Tom VanAntwerp <tom@tomvanantwerp.com>
2019-03-15 11:20:09 -0400

    Content updates

--- src/sections/Details.js
+++ src/sections/Details.js
@@ -117,22 +117,17 @@ const Details = ({ details, id }) => {
         <TaglineContainer id={id} maxWidth={600}>
           <h2>Celebrating 82 Years of Promoting Better Tax Policy</h2>
           <p>
-            Since our founding in 1937, the Tax Foundation has been the nation’s
-            leading independent tax policy nonprofit. Our principled research,
-            insightful analysis, and engaged experts have informed smarter tax
-            policy at the federal, state, and global levels.
+            For 82 years, Tax Prom has been the must-attend event for the tax
+            world’s best, brightest, and most influential.
           </p>
           <p>
-            Our aim in our interactions with policymakers at every level is to
-            guide them towards tax policies that are pro-growth, pro-investment,
-            pro-jobs, and hospitable to the private sector.
+            From Capitol Hill tax writers to VPs of tax at Fortune 500
+            companies, Tax Prom is the one night a year that brings together
+            every major tax policy decision-maker in the country.
           </p>
           <p>
-            For 82 years, the Tax Foundation’s annual dinner has been an
-            institution for the tax community’s best, brightest, and most
-            influential. Join us Thursday, November 21st for our 82nd annual Tax
-            Prom, and find out why hundreds of people say this is the tax
-            world’s most celebrated event of the year.
+            Join us Thursday, November 21st and find out why Tax Prom is an
+            event you can’t afford to miss.
           </p>
         </TaglineContainer>
       </BackgroundColorContainer>
